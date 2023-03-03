9c92e6a49c1275cb0630dfc1e29bc8bd44a733c4
Vlasenko1710 <vlasenko_ea@ukr.net>
2023-03-03 20:51:14 +0200

    add_task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+// створюємо змінні для кнопок
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let colorInterval = null;
+btnStop.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener("click", () => {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+  colorInterval = setInterval(() => {
+      document.body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+btnStop.addEventListener("click", () => {
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+    clearInterval(colorInterval);
+   
+});
 
